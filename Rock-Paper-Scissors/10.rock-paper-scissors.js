635a94371e0ff0889e7985842832b01e8c4e09d4
ritikabhoyar <ritikabhoyar1@gmil.com>
2025-06-25 09:27:38 -0700

    new changes

--- Rock-Paper-Scissors/10.rock-paper-scissors.js
+++ Rock-Paper-Scissors/10.rock-paper-scissors.js
@@ -13,6 +13,7 @@ let score = JSON.parse(localStorage.getItem('score'));
     let isAutoPlaying = false;
     let intervalId;
 
+    
     function autoPlay() {
       if(!isAutoPlaying){
         intervalId =setInterval(function(){
@@ -20,24 +21,69 @@ let score = JSON.parse(localStorage.getItem('score'));
          playGame(playerMove);
         },1000);
         isAutoPlaying = true;
+
+        // When the game is auto playing, change
+       // the text in the button to 'Stop Playing'.
+        document.querySelector('.js-auto-play-button')
+           .innerHTML = 'Stop Playing';
+        
       }else{
          clearInterval(intervalId);
          isAutoPlaying = false;
+
+         // When the game is not playing, change
+        // the text back to 'Auto Play'.
+        document.querySelector('.js-auto-play-button')
+           .innerHTML = 'Auto Play';
       }
-      
     }
+
+    document.querySelector('.js-auto-play-button')
+       .addEventListener('click',() => {
+         autoPlay();
+      });
+
+    document.querySelector('.js-rock-button')
+       .addEventListener('click',() => {
+            playGame('rock');
+      });
+
+    document.querySelector('.js-paper-button')
+        .addEventListener('click',() => {
+         playGame('paper');
+      });
+
+    document.querySelector('.js-scissors-button')
+        .addEventListener('click',() => {
+         playGame('scissors');
+      });
+
+    document.body.addEventListener('keydown',(event) => {
+      if(event.key === 'r'){
+         playGame('rock');
+      }else if(event.key === 'p'){
+         playGame('paper');
+      }else if(event.key === 's'){
+         playGame('scissors');
+      }else if(event.key === 'a'){
+         autoPlay();
+      }else if(event.key === 'Backspace'){
+         //resetScore();
+         showResetConfirmation();
+      }
+    });
 
    function playGame(playerMove){
       const computerMove = pickComputerMove();
 
       let result = '';
 
-      if(playerMove === 'Scissors'){
+      if(playerMove === 'scissors'){
       if(computerMove === 'rock'){
          result = 'You Lose';
       }else if(computerMove === 'paper'){
          result = 'You win';
-      }else if(computerMove === 'Scissors'){
+      }else if(computerMove === 'scissors'){
          result = 'Tie';
       }
 
@@ -46,7 +92,7 @@ let score = JSON.parse(localStorage.getItem('score'));
          result = 'You win';
       }else if(computerMove === 'paper'){
          result = 'Tie';
-      }else if(computerMove === 'Scissors'){
+      }else if(computerMove === 'scissors'){
          result = 'You Lose';
       }
 
@@ -55,7 +101,7 @@ let score = JSON.parse(localStorage.getItem('score'));
          result = 'Tie';
       }else if(computerMove === 'paper'){
          result = 'You Lose';
-      }else if(computerMove === 'Scissors'){
+      }else if(computerMove === 'scissors'){
          result = 'You win';
       }
 
@@ -99,9 +145,53 @@ let score = JSON.parse(localStorage.getItem('score'));
    }else if(randomNumber >= 1/3 && randomNumber < 2/3){
       computerMove = 'paper';
    }else if(randomNumber >= 2/3 && randomNumber < 1){
-      computerMove = 'Scissors';
+      computerMove = 'scissors';
    }
 
    return computerMove;
 
+   }
+
+   function resetScore() {
+      score.wins = 0;
+      score.losses = 0;
+      score.ties = 0;
+      localStorage.removeItem('score');
+      updateScoreElement();
+   }
+
+   document.querySelector('.js-reset-score-button')
+     .addEventListener('click',() => {
+      //resetScore();
+       showResetConfirmation();
+    });
+
+   function showResetConfirmation() {
+      document.querySelector('.js-reset-confirmation')
+        .innerHTML = `
+         Are you sure you want to reset the score?
+         <button class="js-reset-confirm-yes reset-confirm-button">
+         Yes
+         </button>
+
+         <button class="js-reset-confirm-no reset-confirm-button">
+         No
+         </button>
+        `;
+
+        document.querySelector('.js-reset-confirm-yes')
+          .addEventListener('click',() => {
+            resetScore();
+            hideResetConfirmation();
+          });
+
+        document.querySelector('.js-reset-confirm-no')
+           .addEventListener('click',() => {
+            hideResetConfirmation();
+           });
+   }
+
+   function hideResetConfirmation() {
+      document.querySelector('.js-reset-confirmation')
+        .innerHTML = '';
    }
